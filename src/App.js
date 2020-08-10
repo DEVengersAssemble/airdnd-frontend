9649ffe1a0c45038ca5ab8fdb82a55c8295d457b
Leehaeun0 <a01050675111@gmail.com>
2020-08-10 20:41:26 +0900

    :heavy_plus_sign: Add dependency for package.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  //
   return (
     <div className="App">
       <header className="App-header">
