1f2b484162d1248335d14d2af0a699240fefb2b4
Leehaeun0 <a01050675111@gmail.com>
2020-08-11 14:53:22 +0900

    :seedling: Modify files structure

--- src/Routers/MainRouter.js
+++ src/Routers/MainRouter.js
@@ -1,1 +1,31 @@
-//
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import MainPage from '../Pages/MainPage';
+import SearchPage from '../Pages/SearchPage';
+import DetailPage from '../Pages/DetailPage';
+import ReservationPage from '../Pages/ReservationPage';
+import Mypage from '../Pages/Mypage';
+
+const MainRouter = () => {
+  return (
+    <Switch>
+      <Route path="/" component={MainPage} exact />
+      <Route path="/SearchPage" component={SearchPage} />
+      <Route path="/DetailPage" component={DetailPage} exact />
+      <Route path="/ReservationPage" component={ReservationPage} />
+      <Route path="/Mypage" component={Mypage} />
+      <Route
+        render={({ location }) => (
+          <div>
+            <h2 style={{ display: 'inline' }}>
+              이 페이지는 존재하지 않습니다:
+            </h2>
+            <span>{` ${location.pathname}`}</span>
+          </div>
+        )}
+      />
+    </Switch>
+  );
+};
+
+export default MainRouter;
