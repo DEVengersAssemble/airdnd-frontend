8a8c7fc8980f1f033943f5310b7f43a9d874b60e
kimfriendship <kfriendship94@gmail.com>
2020-08-11 15:35:30 +0900

    :wrench: Update eslint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import MainRouter from './Routers/MainRouter';
-import ResetStyle from './StyledComponents/ResetStyle';
 
 function App() {
-<<<<<<< HEAD
-  return <div className="App"></div>;
-=======
   return (
     <>
       <MainRouter />
     </>
   );
->>>>>>> 1f2b484162d1248335d14d2af0a699240fefb2b4
 }
 
 export default App;
