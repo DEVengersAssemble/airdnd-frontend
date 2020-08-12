1c14f65d50d177fd6fbf251422c157fd9e4716e2
kimfriendship <kfriendship94@gmail.com>
2020-08-12 20:51:09 +0900

    :bug: Fix colors of gray

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,13 +1,14 @@
 const theme = {
   main: '#FF385C',
-  gray: '#717171',
   black: '#222222',
+  darkGray: '#717171',
+  gray: '#B0B0B0',
   lightGray: '#F7F7F7',
+  shadow: '#E3E3E3',
   white: '#FFFFFF',
   deepGreen: '#01797E',
   green: '#008489',
   darkRed: '#C03515',
-  shadow: '#E3E3E3',
 
   iPad: `(max-width: 768px)`,
 };
