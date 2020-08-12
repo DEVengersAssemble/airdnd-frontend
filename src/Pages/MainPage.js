b73d512f8e677b2c3839bc66eb52cb96ef189870
JimmyJoo <jimmyjoo1029@gmail.com>
2020-08-12 21:09:22 +0900

    :lipstick: Add OnlineExperience component

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -2,14 +2,15 @@ import React from 'react';
 import MainHeader from '../Components/Main/MainHeader';
 import Background from '../Components/Main/Background';
 import Category from '../Components/Main/Category';
+import OnlineExp from '../Components/Main/OnlineExp';
 
 const MainPage = () => {
   return (
     <div>
       <MainHeader></MainHeader>
-
       <Background></Background>
       <Category></Category>
+      <OnlineExp></OnlineExp>
     </div>
   );
 };
