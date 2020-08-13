31b8621f32f6304e00b849ee66098f44dbde99a0
JimmyJoo <jimmyjoo1029@gmail.com>
2020-08-13 15:00:04 +0900

    :lipstick: Add site links to a tags

--- src/Components/Main/Footer.js
+++ src/Components/Main/Footer.js
@@ -66,6 +66,9 @@ const StContentItemLink = styled.a`
   font-weight: 400;
   font-size: 14px;
   line-height: 18px;
+  &:visited {
+    color: inherit;
+  }
   &:hover {
     text-decoration: underline;
     color: ${props => props.theme.darkGray};
@@ -109,6 +112,9 @@ const StRuleItem = styled.li``;
 const StRuleLink = styled.a`
   text-decoration: none;
   font-size: 14px;
+  &:visited {
+    color: inherit;
+  }
   &:hover {
     text-decoration: underline;
     color: ${props => props.theme.darkGray};
@@ -128,6 +134,7 @@ const StLowerRightWrapper = styled.div`
   @media ${props => props.theme.iPad} {
     text-align: center;
     width: 100%;
+    margin-bottom: 30px;
   }
   display: flex;
   justify-content: center;
@@ -183,6 +190,10 @@ const StSiteLink = styled.a`
   display: flex;
   vertical-align: center;
   cursor: pointer;
+
+  &:visited {
+    color: inherit;
+  }
   &:hover {
     color: ${props => props.theme.darkGray};
   }
@@ -196,33 +207,76 @@ const Footer = () => {
           <StContentTitle>소개</StContentTitle>
           <StContentList>
             <StContentItem>
-              <StContentItemLink>에어비앤비 이용 방법</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/d/howairbnbworks"
+                target="_blank"
+              >
+                에어비앤비 이용 방법
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>뉴스룸</StContentItemLink>
+              <StContentItemLink
+                href="https://news.airbnb.com/ko/"
+                target="_blank"
+              >
+                뉴스룸
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>Airbnb Citizen</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnbcitizen.com/?utm_source=airbnb&utm_medium=footer&utm_campaign=product"
+                target="_blank"
+              >
+                Airbnb Citizen
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>에어비앤비 플러스</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/plus"
+                target="_blank"
+              >
+                에어비앤비 플러스
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>에어비앤비 Luxe</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/luxury"
+                target="_blank"
+              >
+                에어비앤비 Luxe
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>호텔투나잇</StContentItemLink>
+              <StContentItemLink
+                href="https://www.hoteltonight.com/?utm_source_name=Airbnb&utm_campaign_name=Dr_Us_Desktop_Airbnb_Footer"
+                target="_blank"
+              >
+                호텔투나잇
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/work?s=footer"
+                target="_blank"
+              >
                 에어비앤비 비즈니스 프로그램
               </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>올림픽</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/d/olympics"
+                target="_blank"
+              >
+                올림픽
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>채용정보</StContentItemLink>
+              <StContentItemLink
+                href="https://careers.airbnb.com/"
+                target="_blank"
+              >
+                채용정보
+              </StContentItemLink>
             </StContentItem>
           </StContentList>
         </StContentSection>
@@ -230,22 +284,52 @@ const Footer = () => {
           <StContentTitle>커뮤니티</StContentTitle>
           <StContentList>
             <StContentItem>
-              <StContentItemLink>다양성 및 소속감</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/diversity"
+                target="_blank"
+              >
+                다양성 및 소속감
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>접근성</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/d/accessibility"
+                target="_blank"
+              >
+                접근성
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>에어비앤비 매거진</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/magazine"
+                target="_blank"
+              >
+                에어비앤비 매거진
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>에어비앤비 어소시에이트</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/associates?from=footer"
+                target="_blank"
+              >
+                에어비앤비 어소시에이트
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>구호 인력을 위한 숙소</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/d/covid19relief"
+                target="_blank"
+              >
+                구호 인력을 위한 숙소
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>친구 초대하기</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/invite?r=6"
+                target="_blank"
+              >
+                친구 초대하기
+              </StContentItemLink>
             </StContentItem>
           </StContentList>
         </StContentSection>
@@ -253,30 +337,68 @@ const Footer = () => {
           <StContentTitle>호스팅하기</StContentTitle>
           <StContentList>
             <StContentItem>
-              <StContentItemLink>숙소 호스팅</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/host/homes?from_footer=1"
+                target="_blank"
+              >
+                숙소 호스팅
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>온라인 체험 호스팅</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/d/onlinehost"
+                target="_blank"
+              >
+                온라인 체험 호스팅
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>체험 호스팅하기</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/host/experiences"
+                target="_blank"
+              >
+                체험 호스팅하기
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/resources/hosting-homes/t/leadership-updates-35"
+                target="_blank"
+              >
                 브라이언 체스키 CEO의 메시지
               </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>책임감 있는 호스팅</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/help/article/1387/%ED%95%9C%EA%B5%AD%EC%97%90%EC%84%9C-%EC%B1%85%EC%9E%84%EA%B0%90-%EC%9E%88%EB%8A%94-%ED%98%B8%EC%8A%A4%ED%8C%85-%ED%95%98%EA%B8%B0"
+                target="_blank"
+              >
+                책임감 있는 호스팅
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>Open Homes</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/openhomes?from_footer=1"
+                target="_blank"
+              >
+                Open Homes
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>자료 센터</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/resources/hosting-homes"
+                target="_blank"
+              >
+                자료 센터
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>커뮤니티 센터</StContentItemLink>
+              <StContentItemLink
+                href="https://community.withairbnb.com/t5/Community-Center-KR/ct-p/Community-Center-Korean"
+                target="_blank"
+              >
+                커뮤니티 센터
+              </StContentItemLink>
             </StContentItem>
           </StContentList>
         </StContentSection>
@@ -284,19 +406,44 @@ const Footer = () => {
           <StContentTitle>에어비앤비 지원</StContentTitle>
           <StContentList>
             <StContentItem>
-              <StContentItemLink>코로나19 관련 업데이트</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/resources/hosting-homes"
+                target="_blank"
+              >
+                코로나19 관련 업데이트
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>도움말 센터</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/help/home"
+                target="_blank"
+              >
+                도움말 센터
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>예약 취소 옵션</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/help/article/2701/%EC%BD%94%EB%A1%9C%EB%82%98%EB%B0%94%EC%9D%B4%EB%9F%AC%EC%8A%A4%EA%B0%90%EC%97%BC%EC%A6%9D19%EC%BD%94%EB%A1%9C%EB%82%9819-%EA%B4%80%EB%A0%A8-%EC%A0%95%EC%83%81%EC%B0%B8%EC%9E%91%EC%9D%B4-%EA%B0%80%EB%8A%A5%ED%95%9C-%EC%83%81%ED%99%A9-%EC%A0%95%EC%B1%85"
+                target="_blank"
+              >
+                예약 취소 옵션
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>에어비앤비 이웃 민원 지원</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/neighbors"
+                target="_blank"
+              >
+                에어비앤비 이웃 민원 지원
+              </StContentItemLink>
             </StContentItem>
             <StContentItem>
-              <StContentItemLink>신뢰와 안전</StContentItemLink>
+              <StContentItemLink
+                href="https://www.airbnb.co.kr/trust"
+                target="_blank"
+              >
+                신뢰와 안전
+              </StContentItemLink>
             </StContentItem>
           </StContentList>
         </StContentSection>
@@ -308,19 +455,36 @@ const Footer = () => {
           </StCopyRight>
           <StRuleList>
             <StRuleItem>
-              <StRuleLink>개인정보 처리방침</StRuleLink>
+              <StRuleLink
+                href="https://www.airbnb.co.kr/terms/privacy_policy"
+                target="_blank"
+              >
+                개인정보 처리방침
+              </StRuleLink>
             </StRuleItem>
             <StDot aria-hidden="true">・</StDot>
             <StRuleItem>
-              <StRuleLink>이용약관</StRuleLink>
+              <StRuleLink href="https://www.airbnb.co.kr/terms" target="_blank">
+                이용약관
+              </StRuleLink>
             </StRuleItem>
             <StDot aria-hidden="true">・</StDot>
             <StRuleItem>
-              <StRuleLink>사이트맵</StRuleLink>
+              <StRuleLink
+                href="https://www.airbnb.co.kr/sitemaps/v2"
+                target="_blank"
+              >
+                사이트맵
+              </StRuleLink>
             </StRuleItem>
             <StDot aria-hidden="true">・</StDot>
             <StRuleItem>
-              <StRuleLink>한국의 변경된 환불 정책</StRuleLink>
+              <StRuleLink
+                href="https://www.airbnb.co.kr/home/updated_cancellation_policies?korean_strict_policy=true#strict"
+                target="_blank"
+              >
+                한국의 변경된 환불 정책
+              </StRuleLink>
             </StRuleItem>
           </StRuleList>
         </StLowerLeftWrapper>
@@ -339,19 +503,25 @@ const Footer = () => {
           </StSettingButtonWrapper>
           <StSiteLinkList>
             <StSiteLinkItem>
-              <StSiteLink>
+              <StSiteLink
+                href="https://www.facebook.com/AirbnbKorea"
+                target="_blank"
+              >
                 <span className="a11yHidden">페이스북으로 이동하기</span>
                 <FaFacebookF />
               </StSiteLink>
             </StSiteLinkItem>
             <StSiteLinkItem>
-              <StSiteLink>
+              <StSiteLink href="https://twitter.com/airbnb" target="_blank">
                 <span className="a11yHidden">트위터로 이동하기</span>
                 <FaTwitter />
               </StSiteLink>
             </StSiteLinkItem>
             <StSiteLinkItem>
-              <StSiteLink>
+              <StSiteLink
+                href="https://www.instagram.com/airbnb/"
+                target="_blank"
+              >
                 <span className="a11yHidden">인스타그램으로 이동하기</span>
                 <FaInstagram />
               </StSiteLink>
