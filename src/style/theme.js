38aefd1f503e00cee1fe9b53a8ba84b25a477e55
JimmyJoo <jimmyjoo1029@gmail.com>
2020-08-17 00:55:00 +0900

    :recycle: Change theme structure

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,23 +1,22 @@
 const theme = {
-  main: '#FF385C',
-  warning: '#D93900',
-  black: '#222222',
-  darkGray: '#717171',
-  gray: '#B0B0B0',
-  lightGray: '#F7F7F7',
-  shadow: '#E3E3E3',
-  white: '#FFFFFF',
-  deepGreen: '#01797E',
-  green: '#008489',
-  darkRed: '#C03515',
-  lightGreen: '#018A04',
-
-  iPad: 'screen and (max-width: 768px)',
-  medium: 'screen and (max-width: 1127px)',
-  large: 'screen and (max-width: 1200px)',
-
   color: {
-    black: '#333333',
+    main: '#FF385C',
+    warning: '#D93900',
+    black: '#222222',
+    darkGray: '#717171',
+    gray: '#B0B0B0',
+    lightGray: '#F7F7F7',
+    shadow: '#E3E3E3',
+    white: '#FFFFFF',
+    deepGreen: '#01797E',
+    green: '#008489',
+    darkRed: '#C03515',
+    lightGreen: '#018A04',
+  },
+  size: {
+    iPad: 'screen and (max-width: 768px)',
+    medium: 'screen and (max-width: 1127px)',
+    large: 'screen and (max-width: 1200px)',
   },
 };
 
