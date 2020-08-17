497e06cc0b6e10716b7c6580d230918fc7355abb
Leehaeun0 <a01050675111@gmail.com>
2020-08-18 08:13:27 +0900

    :construction: Work in Resevation page progress

--- src/Components/Reservation/HouseRules.js
+++ src/Components/Reservation/HouseRules.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StWrapper = styled.div``;
+
+const StTitle = styled.h2`
+  font-size: 3.2rem;
+  font-weight: 800;
+  line-height: 1.125em;
+`;
 
 const HouseRules = () => {
-  return <div>리뷰</div>;
+  return (
+    <StWrapper>
+      <StTitle>숙소 이용규칙 확인하기</StTitle>
+    </StWrapper>
+  );
 };
 
 export default HouseRules;
