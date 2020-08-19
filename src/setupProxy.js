a8d019f76c35fd172f1d91e8be566db73c355d7a
JimmyJoo <jimmyjoo1029@gmail.com>
2020-08-20 03:41:14 +0900

    :construction: Working on proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api/autocomplete',
+    '/api/v2/',
     createProxyMiddleware({
       target: 'https://www.airbnb.co.kr',
       changeOrigin: true,
