05f6178335dc204b85d246684cb943ed5f1126b8
keemtj <keemgreat@gmail.com>
2020-08-19 13:12:33 +0900

    :construction: Add structure to DetailSubInfoBox.js

--- src/Components/Message/DetailSubInfoBox.js
+++ src/Components/Message/DetailSubInfoBox.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import Profile from '../Global/Profile';
 
 const DetailSubInfoBox = () => {
-  return <div></div>;
+  return (
+    <DtSiWrapper>
+      <DtSiTitle>예약세부정보</DtSiTitle>
+      <DtSiInner>
+        <DtSiGuest>
+          <DtSiGuestText></DtSiGuestText>
+          <DtSiGuestCount></DtSiGuestCount>
+        </DtSiGuest>
+        <DtSiGuestProfileLists>
+          <DtSiGuestProfileItem>
+            <Profile />
+          </DtSiGuestProfileItem>
+          <DtSiGuestProfileItem>
+            <Profile />
+          </DtSiGuestProfileItem>
+        </DtSiGuestProfileLists>
+      </DtSiInner>
+    </DtSiWrapper>
+  );
 };
+
+const DtSiWrapper = styled.div``;
+
+const DtSiTitle = styled.h3``;
+
+const DtSiInner = styled.div``;
+
+const DtSiGuest = styled.div``;
+
+const DtSiGuestText = styled.div``;
+
+const DtSiGuestCount = styled.div``;
+
+const DtSiGuestProfileLists = styled.ul``;
+
+const DtSiGuestProfileItem = styled.li``;
 
 export default DetailSubInfoBox;
