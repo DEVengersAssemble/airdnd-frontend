c76850be6c694e7262ab104b28b37c5c453041f6
kimfriendship <kfriendship94@gmail.com>
2020-08-19 18:13:29 +0900

    :sparkles: Add searchHistory module

--- src/Modules/index.js
+++ src/Modules/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import search from './search';
 import main from './main';
+import searchHistory from './searchHistory';
 
-const rootReducer = combineReducers({ main, search });
+const rootReducer = combineReducers({ main, search, searchHistory });
 
 export default rootReducer;
---
+++ src/Modules/searchHistory.js
@@ -0,0 +1,15 @@
+const initialState = {
+  place: '제주도',
+  dateFrom: '',
+  dateTo: '',
+  traveller: 1,
+};
+
+const searchHistory = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default searchHistory;
