78042239bed15a09042ed13c993494ca02c35730
kimfriendship <kfriendship94@gmail.com>
2020-08-19 15:42:42 +0900

    :bug: Remove map style warning by using google maps with marker

--- src/Components/Search/SearchMap.js
+++ src/Components/Search/SearchMap.js
@@ -21,7 +21,44 @@ const SearchMap = ({ children }) => {
           <MapMarkerButton />
         </StBtnSetWrapper>
       </StStickyWrapper>
-      <Map />
+      {/* <Map /> */}
+      <Map
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCqryK5lMUxY0i_-Zu1cUrgW3_Geg4BrWA&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={
+          <div
+            style={{
+              width: '100%',
+              height: `100vh`,
+              position: 'sticky',
+              top: '0',
+            }}
+          />
+        }
+        containerElement={
+          <div
+            style={{
+              width: '100%',
+              height: `100vh`,
+              position: 'sticky',
+              top: '0',
+            }}
+          />
+        }
+        mapElement={
+          <div
+            style={{
+              width: '100%',
+              height: `100vh`,
+              position: 'sticky',
+              top: '0',
+            }}
+          />
+        }
+
+        // loadingElement={<div style={{ height: `100%` }} />}
+        // containerElement={<div style={{ height: `400px` }} />}
+        // mapElement={<div style={{ height: `100%` }} />}
+      />
     </StWrapper>
   );
 };
