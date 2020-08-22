3dafd19114f6234f4fccbfff8b5f037bf23f18bf
JimmyJoo <jimmyjoo1029@gmail.com>
2020-08-23 07:50:44 +0900

    :construction: Working on Calendar Popup

--- src/Containers/Main/SearchCalendarPopupContainer.js
+++ src/Containers/Main/SearchCalendarPopupContainer.js
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
 
 const SearchCalendarPopupContainer = () => {
-  const []
   // checkIn, checkOut, flexibleDate, currentMonth, monthArr
-  
-  
-  // !checkIn =>
-  // 현재 날짜를 가져온다.
-  // [7, 8, 9, 1]
-  // 
 
   return <div></div>;
 };
