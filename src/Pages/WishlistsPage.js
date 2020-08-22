da8a8f1865c17fb2a97386fa63e0729d7c19f50d
keemtj <keemgreat@gmail.com>
2020-08-23 07:50:39 +0900

    :lipstick: Init WishlistsMain.js

--- src/Pages/WishlistsPage.js
+++ src/Pages/WishlistsPage.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import { darken } from 'polished';
+import { Airbnb } from '@styled-icons/boxicons-logos/Airbnb';
+import WishlistsMain from '../Components/Wishlists/WishlistsMain';
+import Footer from '../Components/Main/Footer';
 
 const WishlistsPage = () => {
-  return <div>저장목록 페이지</div>;
+  return (
+    <>
+      <WishlistsHeaderWrapper>
+        <Header>
+          <Airbnb size="4.5rem" />
+        </Header>
+      </WishlistsHeaderWrapper>
+      <WishlistsMain />
+      <Footer />
+    </>
+  );
 };
+
+const WishlistsHeaderWrapper = styled.div`
+  position: fixed;
+  z-index: 1;
+  background: ${({ theme }) => theme.color.white};
+  width: 100%;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => darken(0.1, theme.color.lightGray)};
+  box-shadow: 1px 0 0.4rem ${({ theme }) => theme.color.shadow};
+  padding-left: 7.5rem;
+  color: ${({ theme }) => theme.color.main};
+  height: 8rem;
+`;
 
 export default WishlistsPage;
