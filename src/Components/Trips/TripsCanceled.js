ab1ffd754148beffe03b20dbb11952ecadf11792
keemtj <keemgreat@gmail.com>
2020-08-22 18:23:51 +0900

    :lipstick: Add Canceled trips button

--- src/Components/Trips/TripsCanceled.js
+++ src/Components/Trips/TripsCanceled.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import { lighten } from 'polished';
+import TripsCanceledItem from './TripsCanceledItem';
+import TripsSubFooter from './TripsSubFooter';
+import Button from '../Global/Button';
 
 const TripsCanceled = () => {
-  return <div>취소된 예약입니다</div>;
+  return (
+    <>
+      <TripsCanceledWrapper>
+        <TripsCanceledLists>
+          <TripsCanceledItem />
+          <TripsCanceledItem />
+          <TripsCanceledItem />
+          <TripsCanceledItem />
+        </TripsCanceledLists>
+      </TripsCanceledWrapper>
+      <TripsSubFooter>
+        환불과 관련하여 질문이 있나요?{' '}
+        <Button btnType="underlined" hover="none" padding="0" fontSize="1.5rem">
+          도움말 센터 방문하기
+        </Button>
+      </TripsSubFooter>
+    </>
+  );
 };
+
+const TripsCanceledWrapper = styled.div`
+  border-top: 1px solid ${({ theme }) => lighten(0.2, theme.color.gray)};
+  border-bottom: 1px solid ${({ theme }) => lighten(0.2, theme.color.gray)};
+  padding-bottom: 3rem;
+`;
+
+const TripsCanceledLists = styled.ul`
+  display: flex;
+  flex-flow: column nowrap;
+`;
 
 export default TripsCanceled;
---
+++ src/Components/Trips/TripsCanceledItem.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { lighten } from 'polished';
+import Button from '../Global/Button';
+import { MdKeyboardArrowRight } from 'react-icons/md';
+
+const TripsCanceledItem = () => {
+  return (
+    <TripsCanceledItemWrapper>
+      <Link to="/schedule/1">
+        <StButton>
+          <TripsCanceledButtonWrapper>
+            <TripsCanceledButtonInnerWrapper>
+              <TripsCanceledButtonImg
+                src="https://a0.muscache.com/im/pictures/a3912086-e317-4913-ab09-fb38e2737ee5.jpg?aki_policy=large"
+                alt="#"
+              />
+              <TripsCanceledTextWrapper>
+                <TripsCanceledTitle>
+                  Round#1, 서울숲 차로 10분 성수역 2호선 보도 8분
+                </TripsCanceledTitle>
+                <TripsCanceledCkLc>
+                  Seongsudong 3(il)-ga, SeongDong-gu, Seoul · 10월 25일 - 10월
+                  26일
+                </TripsCanceledCkLc>
+              </TripsCanceledTextWrapper>
+            </TripsCanceledButtonInnerWrapper>
+            <MdKeyboardArrowRight />
+          </TripsCanceledButtonWrapper>
+        </StButton>
+      </Link>
+    </TripsCanceledItemWrapper>
+  );
+};
+
+const TripsCanceledItemWrapper = styled.li`
+  height: 7.2rem;
+`;
+
+const StButton = styled(Button)`
+  width: 100%;
+  height: 100%;
+  border: none;
+  border-radius: 0px;
+  padding: 0rem 2rem;
+`;
+
+const TripsCanceledButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+const TripsCanceledButtonInnerWrapper = styled.div`
+  display: flex;
+  flex-grow: 1;
+`;
+
+const TripsCanceledButtonImg = styled.img`
+  border-radius: 5px;
+  width: 4rem;
+  height: 4rem;
+`;
+
+const TripsCanceledTextWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: start;
+  justify-content: space-between;
+  padding: 0.15rem 0rem 0.15rem 2rem;
+`;
+
+const TripsCanceledTitle = styled.div`
+  font-size: 1.4rem;
+  font-weight: 400;
+`;
+
+const TripsCanceledCkLc = styled.div`
+  color: ${({ theme }) => lighten(0.1, theme.color.darkGray)};
+  font-size: 1.4rem;
+  font-weight: 400;
+`;
+
+export default TripsCanceledItem;
