997d000e1b96b97651195accfc1523a503698877
keemtj <keemgreat@gmail.com>
2020-08-23 21:41:21 +0900

    :lipstick: Add wishlist card style

--- src/Components/Wishlists/Wishlist.js
+++ src/Components/Wishlists/Wishlist.js
@@ -4,16 +4,30 @@ import styled from 'styled-components';
 const Wishlist = () => {
   return (
     <WishlistCardWrapper>
-      <WishlistImg />
+      <WishlistImgWrapper>
+        <WishlistImg />
+      </WishlistImgWrapper>
     </WishlistCardWrapper>
   );
 };
 
 const WishlistCardWrapper = styled.li`
-  border: 1px solid red;
   width: 40rem;
   height: 30rem;
+  &:nth-child(3n + 2),
+  &:nth-child(3n + 1) {
+    margin-right: 3.3rem;
+  }
+  margin-bottom: 4rem;
+  border-radius: 10px;
+  box-shadow: 0rem 0rem 1rem ${({ theme }) => theme.color.gray};
+  &:hover {
+    /* hover시 자연스러운 shadow증가를 위해 transition효과 필요 */
+    box-shadow: 0rem 0rem 1.5rem ${({ theme }) => theme.color.gray};
+  }
 `;
+
+const WishlistImgWrapper = styled.div``;
 
 const WishlistImg = styled.div`
   border-top-left-radius: 10px;
