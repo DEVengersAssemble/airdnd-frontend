bdb46027cc3fe86da43bd91becea6b985495e954
kimfriendship <kfriendship94@gmail.com>
2020-08-24 02:53:14 +0900

    :sparkles: Create map container component to add logics

--- src/Components/Global/Map.js
+++ src/Components/Global/Map.js
@@ -8,6 +8,40 @@ import {
 } from 'react-google-maps';
 
 const Map = compose(
+  withProps({
+    googleMapURL:
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyCqryK5lMUxY0i_-Zu1cUrgW3_Geg4BrWA&v=3.exp&libraries=geometry,drawing,places',
+    loadingElement: (
+      <div
+        style={{
+          width: '100%',
+          height: `100vh`,
+          position: 'sticky',
+          top: '0',
+        }}
+      />
+    ),
+    containerElement: (
+      <div
+        style={{
+          width: '100%',
+          height: `100vh`,
+          position: 'sticky',
+          top: '0',
+        }}
+      />
+    ),
+    mapElement: (
+      <div
+        style={{
+          width: '100%',
+          height: `100vh`,
+          position: 'sticky',
+          top: '0',
+        }}
+      />
+    ),
+  }),
   withScriptjs,
   withGoogleMap,
 )(props => (
---
+++ src/Containers/Global/MapContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Map from '../../Components/Global/Map';
+import { useSelctor } from 'react-redux';
+
+const MapContainer = () => {
+  // const
+
+  return <Map />;
+};
+
+export default MapContainer;
