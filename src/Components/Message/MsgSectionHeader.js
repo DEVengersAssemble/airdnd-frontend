5614c77c7ef361e32a6168607859a5a77a7a5925
keemtj <keemgreat@gmail.com>
2020-08-27 21:06:05 +0900

    :lipstick: Add border-bottom to header of each sections

--- src/Components/Message/MsgSectionHeader.js
+++ src/Components/Message/MsgSectionHeader.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { darken } from 'polished';
 import theme from '../../style/theme';
 import Button from '../Global/Button';
 import { FaBox } from 'react-icons/fa';
@@ -54,6 +55,7 @@ const MsgSectionHeader = () => {
 };
 
 const MsgSectionHeaderWrapper = styled.div`
+  border-bottom: 1px solid ${({ theme }) => darken(0.1, theme.color.lightGray)};
   display: flex;
   justify-content: space-between;
   align-items: center;
