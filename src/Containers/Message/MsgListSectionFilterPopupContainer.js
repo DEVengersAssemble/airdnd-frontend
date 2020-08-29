de9023098b2f93eca3168d75babfe0ad2c23a0a8
kimfriendship <kfriendship94@gmail.com>
2020-08-30 08:57:05 +0900

    :alembic: Try fixing errors on msg page

--- src/Containers/Message/MsgListSectionFilterPopupContainer.js
+++ src/Containers/Message/MsgListSectionFilterPopupContainer.js
@@ -1,9 +1,7 @@
 import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import MsgListSectionFilterPopup from '../../Components/Message/MsgListSectionFilterPopup';
-import allMsgList from '../../Modules/message';
-import hideMsgList from '../../Modules/message';
-import unreadMsgList from '../../Modules/message';
+import { allMsgList, hideMsgList, unreadMsgList } from '../../Modules/message';
 
 const MsgListSectionFilterPopupContainer = ({
   setOpenPopup,
@@ -23,26 +21,24 @@ const MsgListSectionFilterPopupContainer = ({
   // event
   // ! onClick 이벤트가 발생하면 filter해서 메시지 리스트를 보여준다
   // ! popup을 끈다
-  const onClickFilterList = useCallback(
-    ({ target }) => {
-      // console.dir(target.childNodes[0]);
-      console.dir(target);
-      console.dir(popupRef.current.children[0].children);
-      if (target === popupRef.current.children[0].children[0]) {
-        dispatch(allMsgList());
-        setOpenPopup(false);
-      }
-      if (target === popupRef.current.children[0].children[1]) {
-        dispatch(hideMsgList());
-        setOpenPopup(false);
-      }
-      if (target === popupRef.current.children[0].children[3]) {
-        dispatch(unreadMsgList());
-        setOpenPopup(false);
-      }
-    },
-    [dispatch, setOpenPopup],
-  );
+  const onClickFilterList = ({ target }) => {
+    console.dir(target.childNodes[0]);
+    console.dir(target);
+    console.dir(popupRef.current.children[0].children[0]);
+    if (target === popupRef.current.children[0].children[0].children[0]) {
+      console.log('true');
+      dispatch(allMsgList());
+      setOpenPopup(false);
+    }
+    if (target === popupRef.current.children[0].children[1]) {
+      dispatch(hideMsgList());
+      setOpenPopup(false);
+    }
+    if (target === popupRef.current.children[0].children[3]) {
+      dispatch(unreadMsgList());
+      setOpenPopup(false);
+    }
+  };
 
   return (
     <MsgListSectionFilterPopup
