7dc12a90730098032e1c9bb9c0dd0bf8c4a6f1a7
kimfriendship <kfriendship94@gmail.com>
2020-08-31 01:10:06 +0900

    :alembic: Experiment getting data from server

--- src/Containers/Search/SearchContentContainer.js
+++ src/Containers/Search/SearchContentContainer.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import SearchContent from '../../Components/Search/SearchContent';
+import axios from 'axios';
 
 const SearchContentContainer = () => {
+  useEffect(() => {
+    const test = async () => {
+      try {
+        const response = await axios.get('back/initialState');
+        console.log(response);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    test();
+  }, []);
+
   return <SearchContent />;
 };
 
