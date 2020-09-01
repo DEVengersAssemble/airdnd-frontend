08f54066c9ee0b7a8adb93343bffa5c1327db609
kimfriendship <kfriendship94@gmail.com>
2020-09-01 18:43:44 +0900

    :sparkles: Add getting query feature

--- src/Containers/Search/SearchContentContainer.js
+++ src/Containers/Search/SearchContentContainer.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import SearchContent from '../../Components/Search/SearchContent';
+import qs from 'qs';
 
 const SearchContentContainer = () => {
+  const query = useLocation();
+  const { location, checkIn, checkOut } = qs.parse(query.search, {
+    ignoreQueryPrefix: true,
+  });
+
   return <SearchContent />;
 };
 
