0fde5464aef88a70a8d88c783d223f05361cead8
keemtj <keemgreat@gmail.com>
2020-09-02 01:22:41 +0900

    :seedling: Add MsgDetailSectionMainContainer file

---
+++ src/Containers/Message/MsgDetailSectionMainContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MsgDetailSectionMain from '../../Components/Message/MsgDetailSectionMain';
+
+const MsgDetailSectionMainContainer = () => {
+  return <MsgDetailSectionMain />;
+};
+
+export default MsgDetailSectionMainContainer;
