55c510f7eb24bc0adab16ac14db8bd6791fdaf9f
JimmyJoo <jimmyjoo1029@gmail.com>
2020-09-01 16:33:42 +0900

    :art: Add autocomplete api

--- src/Api/searchFormApi.js
+++ src/Api/searchFormApi.js
@@ -1,15 +1,6 @@
 import axios from 'axios';
 
-const homes = axios.create();
 export const getLocationAutoComplete = async value => {
-  // const results = await axios.get(
-  //   `/api/v2/autocompletes?country=KR&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&language=ko&locale=ko&num_results=5&user_input=${value}&api_version=1.1.1&satori_config_token=EhIiJQIiEhUCEiIyEhIyEiEA&vertical_refinement=all&region=-1&options=should_filter_by_vertical_refinement%7Chide_nav_results%7Cshould_show_stays%7Csimple_search`,
-  // );
-  const res = await homes.get(
-    `back/initialState/location/${'seoul'}/checkIn/${'2020-09-02'}/checkOut/${'2020-09-06'}/adults/${'3'}`,
-  );
-  console.log('res: ', res);
-  // const results = await axios.get(`/back/autocomplete/json/${value}`);
-  // return results.data.autocomplete_terms.map(term => term.display_name);
-  return [];
+  const { data } = await axios.get(`/back/autocomplete/json/${value}`);
+  return Object.values(data);
 };
