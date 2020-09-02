e63b314cc4a2a8cac78f9bf64cdf9f177b208619
kimfriendship <kfriendship94@gmail.com>
2020-09-03 00:04:40 +0900

    :sparkles: Add home url on link tag

--- src/Containers/Search/HomePopupContainer.js
+++ src/Containers/Search/HomePopupContainer.js
@@ -1,28 +1,22 @@
 import React, { useState } from 'react';
 import HomePopup from '../../Components/Search/HomePopup';
-import { hoverHome, blurHome, changeHeart } from '../../Modules/search';
+import { changeHeart } from '../../Modules/search';
 import { removeBookmark, openListModal } from '../../Modules/wishlists';
 
 const HomePopupContainer = ({ home, dateDiff, theme, store }) => {
-  // const { hoveredHome } = store.getState().search;
   const { id } = store.getState().user;
   const dispatch = store.dispatch;
   const [isHovered, setIsHovered] = useState(false);
   const onHoverHome = () => setIsHovered(true);
   const onBlurHome = () => setIsHovered(false);
 
-  // const onHoverHome = () =>
-  // hoveredHome !== home.homeId && dispatch(hoverHome(home.homeId));
-  // const onBlurHome = () => dispatch(blurHome());
   const onClickHeart = () => {
     if (!id) return console.log('로그인 해라~~~~~~~~~~~~~ 로그인 팝업 띄우기');
     if (home.isBookmarked) {
-      console.log('북마크 없앤다니까 왜 모달 열어', home.isBookmarked);
       dispatch(removeBookmark(home.homeId));
       dispatch(changeHeart(home.homeId));
       return;
     }
-    console.log('북마크', home.isBookmarked);
     dispatch(openListModal(home.homeId, home.imageArray[0]));
   };
 
