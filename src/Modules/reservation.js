2395f2fb9e84398dca41c0783ba7e5a0fb18b489
Leehaeun0 <a01050675111@gmail.com>
2020-09-02 22:41:45 +0900

    :sparkles: Add feature to controll calendar state

--- src/Modules/reservation.js
+++ src/Modules/reservation.js
@@ -1,12 +1,15 @@
 const SET_CHECKIN = 'reservation/SET_CHECKIN';
 const SET_CHECKOUT = 'reservation/SET_CHECKOUT';
 const SET_GUESTS = 'reservation/SET_GUESTS';
+const DELETE_CHECKINOUT = 'reservation/DELETE_CHECKINOUT';
 
 export const setCheckin = checkin => ({ type: SET_CHECKIN, checkin });
 export const setCheckout = checkout => ({ type: SET_CHECKOUT, checkout });
 export const setGuests = guests => ({ type: SET_CHECKIN, guests });
+export const deleteCheckInOut = () => ({ type: DELETE_CHECKINOUT });
 
 const getDateDiff = (date1, date2) => {
+  if (!date1 || !date2) return 0;
   const checkIn = new Date(date1);
   const checkOut = new Date(date2);
   const timeDiff = checkOut.getTime() - checkIn.getTime();
@@ -14,9 +17,10 @@ const getDateDiff = (date1, date2) => {
 };
 
 const initialState = {
-  checkin: '2020.09.01',
+  checkin: '',
   checkout: '',
-  dateDiff: getDateDiff('2020.09.01', '2020.09.05'),
+  dateDiff: 0,
+  // dateDiff: getDateDiff('2020.08.15', ''2020.08.18),
   reservedDates: ['2020.08.15', '2020.08.16', '2020.08.20', '2020.09.25'],
   guests: { adult: 0, child: 0, infant: 0 },
   totalGuest: 0,
@@ -28,12 +32,22 @@ const reservation = (state = initialState, action) => {
       return {
         ...state,
         checkin: action.checkin,
+        dateDiff: getDateDiff(action.checkin, state.checkout),
       };
     }
     case SET_CHECKOUT: {
       return {
         ...state,
         checkout: action.checkout,
+        dateDiff: getDateDiff(state.checkin, action.checkout),
+      };
+    }
+    case DELETE_CHECKINOUT: {
+      return {
+        ...state,
+        checkin: '',
+        checkout: '',
+        dateDiff: 0,
       };
     }
     case SET_GUESTS: {
