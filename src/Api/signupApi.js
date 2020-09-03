94df3d654d722a8b3708840174cb8c9a175f2e1f
JimmyJoo <jimmyjoo1029@gmail.com>
2020-09-03 15:07:54 +0900

    :construction: Working on sign up api

--- src/Api/signupApi.js
+++ src/Api/signupApi.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
+const sleep = n => new Promise(resolve => setTimeout(resolve, n));
+
+const testData = 'true';
 
 export const sendSignUpReq = async payload => {
-  const { data } = await axios.post('/back/signUp', payload);
-  return data;
+  // const { data } = await axios.post('/back/signUp', payload);
+  sleep(1500);
+  return testData;
 };
