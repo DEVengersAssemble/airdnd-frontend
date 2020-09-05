aadcca3944252dbdd50a6492def2a6a389869c71
JimmyJoo <jimmyjoo1029@gmail.com>
2020-09-05 14:18:36 +0900

    :sparkles: Add a feature to get date diff from checkin and checkout

--- src/Containers/Main/SearchFormContainer.js
+++ src/Containers/Main/SearchFormContainer.js
@@ -17,20 +17,42 @@ const SearchFormContainer = ({ isSearchBtnClicked }) => {
     setType(() => type);
   };
 
-  const getDateDiff = (date1, date2) => {
-    const checkIn = new Date(date1);
-    const checkOut = new Date(date2);
-    const timeDiff = checkOut.getTime() - checkIn.getTime();
-    return Math.ceil(timeDiff / (1000 * 3600 * 24));
-  };
-
   const handleSubmit = e => {
     e.preventDefault();
-    const { location, checkIn, checkOut, flexibleDate, guests } = searchData;
+    const {
+      location,
+      checkIn,
+      checkOut,
+      dateDiff,
+      flexibleDate,
+      guests,
+    } = searchData;
+    let newCheckIn = checkIn;
+    let newCheckOut = checkOut;
+    if (checkIn && !checkOut) {
+      const checkInDate = new Date(checkIn);
+      const checkOutDate = new Date();
+      checkOutDate.setDate(checkInDate.getDate() + 1);
+      const year = checkOutDate.getFullYear();
+      const month = `0${checkOutDate.getMonth() + 1}`.slice(-2);
+      const date = `0${checkOutDate.getDate()}`.slice(-2);
+      const payload = `${year}.${month}.${date}`;
+      newCheckOut = payload;
+      changeSearchData('checkOut', payload);
+    } else if (!checkIn && checkOut) {
+      const checkOutDate = new Date(checkIn);
+      const checkInDate = new Date();
+      checkInDate.setDate(checkOutDate.getDate() - 1);
+      const year = checkOutDate.getFullYear();
+      const month = `0${checkOutDate.getMonth() + 1}`.slice(-2);
+      const date = `0${checkOutDate.getDate()}`.slice(-2);
+      const payload = `${year}.${month}.${date}`;
+      newCheckIn = payload;
+      changeSearchData('checkIn', payload);
+    }
     const { adult, child, infant } = guests;
     const guestCount = adult + child + infant;
-    const dateDiff = getDateDiff(checkIn, checkOut);
-    const url = `/search?location=${location}&checkIn=${checkIn}&checkOut=${checkOut}&dateDiff=${dateDiff}&flexibleDate=${flexibleDate}&guests=${guestCount}&adult=${adult}&child=${child}&infant=${infant}`;
+    const url = `/search?location=${location}&checkIn=${newCheckIn}&checkOut=${newCheckOut}&dateDiff=${dateDiff}&flexibleDate=${flexibleDate}&guests=${guestCount}&adult=${adult}&child=${child}&infant=${infant}`;
     history.push(url);
     window.scrollTo({ top: 0 });
   };
@@ -50,24 +72,22 @@ const SearchFormContainer = ({ isSearchBtnClicked }) => {
 
   const changeAutoComplete = value => {
     if (!value) {
-      dispatch(setSearchData({ name: 'location', value: '' }));
+      changeSearchData('location', '');
       setLocationResult([]);
       return;
     } else {
-      dispatch(setSearchData({ name: 'location', value }));
+      changeSearchData('location', value);
       debounceGetAutoCompleteResult(value);
       setType('location');
     }
   };
 
   const setCheckIn = date => {
-    console.log('[setCheckIn]: ', date);
     changeSearchData('checkIn', date);
     changeType('checkOut');
   };
 
   const setCheckOut = date => {
-    console.log('[setCheckOut]: ', date);
     changeSearchData('checkOut', date);
     changeType('guests');
   };
@@ -123,15 +143,14 @@ const SearchFormContainer = ({ isSearchBtnClicked }) => {
     guestsResetBtnRef,
   };
 
-  console.log(checkInWrapperRef);
   const changeFocus = () => {
     if (type === 'checkIn') checkInWrapperRef.current.focus();
     else if (type === 'checkOut') checkOutWrapperRef.current.focus();
     else if (type === 'guests') guestsWrapperRef.current.focus();
   };
 
-  const handlePopup = ({ target }) => {
-    console.log('handlePopup...', type);
+  const handlePopup = ({ target }, clickType) => {
+    console.log('handlePopup...', clickType);
     if (locationListRef.current && locationListRef.current.contains(target)) {
       console.log(1);
       changeType('checkIn');
@@ -160,39 +179,39 @@ const SearchFormContainer = ({ isSearchBtnClicked }) => {
         !guestsResetBtnRef.current.contains(target))
     ) {
       console.log(5);
-      console.log(
-        type === 'location' &&
-          !locationResetBtnRef.current.contains(target) &&
-          !locationWrapperRef.current.contains(target),
-      );
-      console.log(
-        type === 'checkIn' &&
-          !calendarPopupRef.current.contains(target) &&
-          !checkInWrapperRef.current.contains(target),
-      );
-      console.log(
-        type === 'checkOut' &&
-          !calendarPopupRef.current.contains(target) &&
-          !checkOutWrapperRef.current.contains(target),
-      );
-      console.log(
-        type === 'guests' &&
-          !guestsPopupRef.current.contains(target) &&
-          !guestsResetBtnRef.current.contains(target),
-      );
-      console.log('close...');
+      // console.log(
+      //   type === 'location' &&
+      //     !locationResetBtnRef.current.contains(target) &&
+      //     !locationWrapperRef.current.contains(target),
+      // );
+      // console.log(
+      //   type === 'checkIn' &&
+      //     !calendarPopupRef.current.contains(target) &&
+      //     !checkInWrapperRef.current.contains(target),
+      // );
+      // console.log(
+      //   type === 'checkOut' &&
+      //     !calendarPopupRef.current.contains(target) &&
+      //     !checkOutWrapperRef.current.contains(target),
+      // );
+      // console.log(
+      //   type === 'guests' &&
+      //     !guestsPopupRef.current.contains(target) &&
+      //     !guestsResetBtnRef.current.contains(target),
+      // );
+      // console.log('close...');
       changeType(null);
     }
   };
 
   useEffect(() => {
     console.log('2...useEffect...type: ', type);
-    document.addEventListener('click', handlePopup);
+    document.addEventListener('click', e => handlePopup(e, type));
     type && changeFocus();
     return () => {
-      document.removeEventListener('click', handlePopup);
+      document.removeEventListener('click', e => handlePopup(e, type));
     };
-  }, [type, handlePopup]);
+  }, [type]);
 
   return (
     <SearchForm
