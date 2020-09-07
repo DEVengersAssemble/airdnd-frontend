b825febe08a9674cd28382573b68fc9ce71f195a
keemtj <keemgreat@gmail.com>
2020-09-08 01:27:26 +0900

    :construction: Fix props

--- src/Routers/TripsSubRouter.js
+++ src/Routers/TripsSubRouter.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import TripsUpcomingContainer from '../Containers/Trips/TripsUpcomingContainer';
-import TripsPastContainer from '../Containers/Trips/TripsPastContainer';
-import TripsCanceledContainer from '../Containers/Trips/TripsCanceledContainer';
 
 const SubRouter = () => {
   return (
     <Switch>
       <Route exact path="/trips/v1" component={TripsUpcomingContainer} />
-      {/* <Route path="/trips/v1?tab=upcoming" component={TripsUpcomingContainer} /> */}
-      {/* <Route path="/trips/v1?tab=past" component={TripsPastContainer} /> */}
-      {/* <Route path="/trips/v1?tab=canceled" component={TripsCanceledContainer} /> */}
       <Route
         render={({ location }) => (
           <div>
