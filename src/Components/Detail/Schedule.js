3b6ea051d78a69535231ea624efd83fe509fc2c8
Leehaeun0 <a01050675111@gmail.com>
2020-09-10 08:57:21 +0900

    :art: Divide component and Add css

--- src/Components/Detail/Schedule.js
+++ src/Components/Detail/Schedule.js
@@ -2,9 +2,9 @@ import React from 'react';
 // import { useDispatch } from 'react-redux';
 import Division from './Division';
 import styled from 'styled-components';
-import Button from '../Global/Button';
 import DetailCalendarContainer from '../../Containers/Detail/DetailCalendarContainer';
 import CalendarDeleteBtnContainer from '../../Containers/Detail/CalendarDeleteBtnContainer';
+import CalendarKeybordBtnContainer from '../../Containers/Detail/CalendarKeybordBtnContainer';
 // import {
 //   deleteCheckInOut,
 //   setChangeInitialDate,
@@ -24,7 +24,8 @@ const Schedule = () => {
         여행 날짜를 입력하여 정확한 요금을 확인하세요.
       </StTravelTerm>
       <DetailCalendarContainer />
-      <StKeybordBtn transition transScale="0.88">
+      <CalendarKeybordBtnContainer />
+      {/* <StKeybordBtn transition transScale="0.88">
         <svg
           aria-hidden="true"
           role="presentation"
@@ -34,7 +35,7 @@ const Schedule = () => {
         >
           <path d="M29 5a2 2 0 0 1 1.995 1.85L31 7v18a2 2 0 0 1-1.85 1.995L29 27H3a2 2 0 0 1-1.995-1.85L1 25V7a2 2 0 0 1 1.85-1.995L3 5zm0 2H3v18h26zm-8 13v2H11v-2zm3-5a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm-4 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm-4 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm-4 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm-4 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm16-4a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm-4 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm-4 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm-4 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm-4 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
         </svg>
-      </StKeybordBtn>
+      </StKeybordBtn> */}
       <CalendarDeleteBtnContainer />
       {/* <StDeleteDate btnType="underlined" transition onClick={onClick}>
         날짜 지우기
@@ -50,19 +51,19 @@ const StTravelTerm = styled.div`
   color: ${({ theme }) => theme.color.darkGray};
 `;
 
-const StKeybordBtn = styled(Button)`
-  width: 48px;
-  height: 48px;
-  padding: 0;
-  border: none;
-  border-radius: 50%;
+// const StKeybordBtn = styled(Button)`
+//   width: 48px;
+//   height: 48px;
+//   padding: 0;
+//   border: none;
+//   border-radius: 50%;
 
-  svg {
-    height: 24px;
-    width: 24px;
-    fill: currentcolor;
-  }
-`;
+//   svg {
+//     height: 24px;
+//     width: 24px;
+//     fill: currentcolor;
+//   }
+// `;
 
 // const StDeleteDate = styled(Button)`
 //   vertical-align: super;
