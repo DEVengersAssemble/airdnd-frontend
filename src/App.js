b68332c33f3d34ff04e913f4149f7d75344e8250
JimmyJoo <jimmyjoo1029@gmail.com>
2020-09-11 00:17:38 +0900

    :heavy_plus_sign: Add react-cookie

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MainRouter from './Routers/MainRouter';
 import ResetStyle from './style/ResetStyle';
+import { useCookies } from 'react-cookie';
 
 function App() {
+  console.log(' --- [App] --- ');
+  const [cookies, setCookie, removeCookie] = useCookies(['AirdndSES']);
+  console.log(cookies['AirdndSES']);
+  useEffect(() => {
+    console.log('cookie: ', cookies);
+  }, []);
+
   return (
     <>
       <MainRouter />
