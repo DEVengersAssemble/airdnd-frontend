149d661940ac8a8e9c15aed750970ed19039e460
JimmyJoo <jimmyjoo1029@gmail.com>
2020-09-11 20:47:53 +0900

    :fire: Remove untracked files

--- src/Containers/Main/SearchCalendarPopupContainer.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect, useState, useCallback } from 'react';
-
-const SearchCalendarPopupContainer = () => {
-  // checkIn, checkOut, flexibleDate, currentMonth, monthArr
-
-  return <div></div>;
-};
-
-export default SearchCalendarPopupContainer;
