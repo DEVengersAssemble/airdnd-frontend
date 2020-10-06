681f9d1701ad11e4c51cadcf3e45a5932f1a6a7f
Leehaeun0 <a01050675111@gmail.com>
2020-10-06 23:45:55 +0900

    :recycle: Refactoring

--- src/Modules/home.js
+++ src/Modules/home.js
@@ -5,12 +5,15 @@ const GET_HOME = 'home/GET_HOME';
 const GET_HOME_SUCCESS = 'home/GET_HOME_SUCCESS';
 const GET_HOME_ERROR = 'home/GET_HOME_ERROR';
 const RESIZE_SCREEN = 'home/RESIZE_SCREEN';
+const TOGGLE_IS_BOOKMARKED = 'home/TOGGLE_IS_BOOKMARKED';
 
 export const getHome = id => async dispatch => {
   dispatch({ type: GET_HOME }); // 요청이 시작됨
   try {
     const home = await homeApi.getHome(id); // Api 호출
-    dispatch({ type: GET_HOME_SUCCESS, home }); // 성공
+    setTimeout(() => {
+      dispatch({ type: GET_HOME_SUCCESS, home }); // 성공
+    }, 800);
   } catch (e) {
     dispatch({ type: GET_HOME_ERROR, error: e }); // 실패
   }
@@ -20,6 +23,8 @@ export const onResize = () => {
   console.log('resizing...');
   return { type: RESIZE_SCREEN };
 };
+
+export const toggleIsBookmarked = () => ({ type: TOGGLE_IS_BOOKMARKED });
 
 const initialState = {
   homeState: { isLoading: false, home: null, error: null },
@@ -40,7 +45,15 @@ const home = (state = initialState, action) => {
     case GET_HOME_SUCCESS:
       return {
         ...state,
-        homeState: { isLoading: false, home: action.home, error: null },
+        // homeState: { isLoading: false, home: action.home, error: null },
+        homeState: {
+          isLoading: false,
+          home: {
+            ...action.home,
+            isBookmarked: action.home.isBookmarked === 'true' ? true : false,
+          },
+          error: null,
+        },
       };
     case GET_HOME_ERROR:
       return {
@@ -56,6 +69,17 @@ const home = (state = initialState, action) => {
             .matches,
           isScreenLarge: window.matchMedia('screen and (max-width: 1200px)')
             .matches,
+        },
+      };
+    case TOGGLE_IS_BOOKMARKED:
+      return {
+        ...state,
+        homeState: {
+          ...state.homeState,
+          home: {
+            ...state.homeState.home,
+            isBookmarked: !state.homeState.home.isBookmarked,
+          },
         },
       };
     default:
