66413d7cf54af9541bd77966fe7d1f4968f52fec
Leehaeun0 <a01050675111@gmail.com>
2020-10-15 22:50:12 +0900

    :sparkles: Modify a function to run differently for each page

--- src/lib/bookmarkUtils.js
+++ src/lib/bookmarkUtils.js
@@ -1,15 +1,13 @@
 import { openModal } from '../Modules/modal';
 import { changeHeart } from '../Modules/search';
+import { toggleIsBookmarked } from '../Modules/home';
 import { removeBookmark, openListModal } from '../Modules/wishlists';
 
 export const toggleBookmark = (userId, home, dispatch) => {
   const homeId = home.homeId || home.id;
-  const imageArray = home.imageArray;
-  console.log(imageArray);
-  const isBookmarked =
-    typeof home.isBookmarked === 'boolean' ? home.isBookmarked : false;
+  const imageArray = home.imageArray || home.images;
   if (!userId) return dispatch(openModal('login'));
-  if (isBookmarked) {
+  if (home.isBookmarked) {
     dispatch(removeBookmark(homeId));
     return;
   }
@@ -19,13 +17,20 @@ export const toggleBookmark = (userId, home, dispatch) => {
 export const fetchBookmarkDataThunk = (type, promiseCreator) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
 
-  return param => async dispatch => {
+  return param => async (dispatch, getState) => {
     console.log(param);
+    const { home } = getState();
     dispatch({ type });
     try {
       const payload = await promiseCreator(param); // API 호출
       dispatch({ type: SUCCESS, payload, param }); // 호출 성공
-      dispatch(changeHeart(param.bookmarkHomeId));
+
+      // state의 isBookmarked를 바꾸는 함수를 페이지에 맞게 호출함
+      if (home.homeState.home) {
+        dispatch(toggleIsBookmarked());
+      } else {
+        dispatch(changeHeart(param.bookmarkHomeId));
+      }
     } catch (e) {
       dispatch({ type: ERROR, payload: e, error: true }); // 호출 실패
     }
